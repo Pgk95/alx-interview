0026b7ffe78a9438468e661d7c5b01a0dd7f6a9b
pgk95 <kalupgk@gmail.com>
2023-10-26 11:16:20 +0300

    quick fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,15 @@
 
 def validUTF8(byte) -> bool:
     """Checks if byte is valid"""
+
     if byte[0] & 0b10000000 == 0b00000000:
         return True
-    return False
+
+    if byte[0] & 0b11100000 == 0b11000000:
+        if byte[1] & 0b11000000 == 0b10000000:
+            return True
+
+    if byte[0] & 0b11100000 == 0b11100000:
+        return False
+
+    return True
