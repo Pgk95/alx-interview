1e61aa6bbca7dfd73539994b794b886d3748dde4
pgk95 <kalupgk@gmail.com>
2023-10-26 13:23:01 +0300

    quick fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,12 +2,24 @@
 """UTF-8 Validation"""
 
 
-def validUTF8(data) -> bool:
+def validUTF8(data)-> bool:
     """Checks if byte is valid"""
-    if data == [229, 127, 256]:
-        return False
-    try:
-        bytes(data).decode()
-    except BaseException:
-        return False
-    return True
+
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 5) == 0b110 or (byte >> 5) == 0b1110:
+                num_bytes = 1
+            elif (byte >> 7) == 0b1:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
