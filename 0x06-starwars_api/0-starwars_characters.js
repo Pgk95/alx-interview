8582d20835cf5acf19520c0e7edd2281bba41098
pgk95 <kalupgk@gmail.com>
2023-11-09 08:33:13 +0300

    quick fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,21 +2,21 @@
 const request = require('request');
 const MovieID = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + MovieID;
-const methiod = 'GET';
+const method = 'GET';
 
-request(url, methiod, function (error, response, body) {
+request(url, method, function (error, response, body) {
     if (error) {
         console.log(error);
     } else {
         const characters = JSON.parse(body).characters;
         for (const character of characters) {
-        request(character, methiod, function (error, response, body) {
-            if (error) {
-            console.log(error);
-            } else {
-            console.log(JSON.parse(body).name);
-            }
-        });
+            request(character, method, function (error, response, body) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(JSON.parse(body).name);
+                }
+            });
         }
     }
 });
