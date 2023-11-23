9daf8862d1cd8fce5a9eb71659a0d4d0b4946858
pgk95 <kalupgk@gmail.com>
2023-11-23 10:40:58 +0300

    task done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""making change"""
+
+
+def makeChange(coins, total):
+    """makeChange"""
+    if total <= 0:
+        return 0
+    if total in coins:
+        return 1
+    
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if coin <= total:
+            num_coins += total // coin
+            total = total % coin
+        if total == 0:
+            break
+    if total != 0:
+        return -1
+    return num_coins
