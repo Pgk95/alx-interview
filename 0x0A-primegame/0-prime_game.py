5f6337a8563e4d2b5c1083041549cc584830324a
pgk95 <kalupgk@gmail.com>
2023-12-06 14:31:56 +0300

    quick fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,10 +7,10 @@ def primes(n):
     prime = []
     filter = [True] * (n + 1)
     for p in range(2, n + 1):
-        if (sieve[p]):
+        if (filter[p]):
             prime.append(p)
             for i in range(p, n + 1, p):
-                sieve[i] = False
+                filter[i] = False
     return prime
 
 
